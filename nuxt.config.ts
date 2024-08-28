c1f180009ce2d9eab56233f6f0d2beb78493edda
broadsign-lekevoid <kevin.gagnon@broadsign.com>
2024-08-28 17:57:17 -0400

    Tweak

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				compatibilityDate: "2024-04-03",
-				devtools: { enabled: true },
-				modules: ["@nuxt/ui", "@vite-pwa/nuxt"],
+	compatibilityDate: "2024-04-03",
+	devtools: { enabled: true },
+	modules: ["@nuxt/ui", "@vite-pwa/nuxt"],
 });
